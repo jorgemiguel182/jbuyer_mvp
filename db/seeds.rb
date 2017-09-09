da4bff6396fe27dbafb087964d40315eb3d35374
jmdinfo123 <jmdinfo123@gmail.com>
2017-09-09 09:31:36 -0400

    seed cria adm

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #Cadastrando Administrador
-#puts "Cadastrando o ADMINISTRADOR..."
+puts "Cadastrando o ADMINISTRADOR..."
 
-#Admin.create!(
- #   email: "admin@admin.com",
-  #  password: "123456",
-   # password_confirmation: "123456")
+Admin.create!(
+    email: "admin@admin.com",
+    password: "123456",
+    password_confirmation: "123456")
 
-#puts "ADMINISTRADOR cadastrado com sucesso!"
+puts "ADMINISTRADOR cadastrado com sucesso!"
 
 
 
@@ -29,15 +29,15 @@
 
 
 # Cadastrando pedido
-puts "Cadastrando PEDIDO ..."
+#puts "Cadastrando PEDIDO ..."
 
-x = Produto.create!(produto: "teste3", valor: 1.2)  # O create já cria e salva o produto
-y = Produto.create!(produto: "teste4", valor: 2.3)
+#x = Produto.create!(produto: "teste3", valor: 1.2)  # O create já cria e salva o produto
+#y = Produto.create!(produto: "teste4", valor: 2.3)
 
-z = Pedido.new(status: 1)
-z.produtos << x
-z.produtos << y
-z.save!
+##z = Pedido.new(status: 1)
+#z.produtos << x
+#z.produtos << y
+#z.save!
 
-puts "PEDIDO cadastrado com sucesso"
+#puts "PEDIDO cadastrado com sucesso"
 
