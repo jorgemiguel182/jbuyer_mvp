74729d25ed9df6e7f2d1723728a219583d63c593
jmdinfo123 <jmdinfo123@gmail.com>
2017-09-18 22:05:54 -0400

    CPF do user de INT para FLOAT

--- db/migrate/20170916033113_create_users.rb
+++ db/migrate/20170916033113_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :nome
-      t.integer :cpf
+      t.string :cpf
       t.string :tel_fixo
       t.string :tel_cel
       t.string :email
