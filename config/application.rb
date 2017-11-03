8aed78ea0d45aee79434b0140a05a6f3b8bb8bfa
jmdinfo123 <jmdinfo123@gmail.com>
2017-11-02 21:47:29 -0400

    arrumando hora timezone bd

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Teste
   class Application < Rails::Application
+    config.time_zone = 'Brasilia'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +22,7 @@ module Teste
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w( application )
+    
     
 config.middleware.insert_before 0, "Rack::Cors" do
       allow do
