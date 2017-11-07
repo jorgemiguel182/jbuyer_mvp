3ba5e7db1d6d53cf8d0760c0c481e5a0fef4c88d
jmdinfo123 <jmdinfo123@gmail.com>
2017-11-06 21:24:02 -0500

    primeiros passos ATENTICACAO JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ActiveRecord::Base
+  before_create -> {self.token = generate_token}
+  
   has_many :pedidos
   belongs_to :endereco
+  
   accepts_nested_attributes_for :endereco
   
  # scope :co_pedido, ->  User.joins(:pedido)
   
+  private
+ 
+  def generate_token
+      loop do
+        token = SecureRandom.hex
+        return token unless User.exists?({token: token})
+      end
+  end
   
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918223808) do
+ActiveRecord::Schema.define(version: 20171107022016) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -96,8 +96,10 @@ ActiveRecord::Schema.define(version: 20170918223808) do
     t.integer  "endereco_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
   end
 
   add_index "users", ["endereco_id"], name: "index_users_on_endereco_id"
+  add_index "users", ["token"], name: "index_users_on_token"
 
 end
