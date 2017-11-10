7c5b589dce2bfc4b883e37bdb46adf5f44e40679
jmdinfo123 <jmdinfo123@gmail.com>
2017-11-10 07:43:46 -0500

    a

--- app/controllers/backoffice/produtos_controller.rb
+++ app/controllers/backoffice/produtos_controller.rb
@@ -3,9 +3,11 @@ class Backoffice::ProdutosController < BackofficeController
 
   
   def index
-    respond_to do |format|
-      @produtos = Produto.all.paginate(:page => params[:page], :per_page => 5)
-      format.json { render json: {produtos: @produtos} }
+    @produtos = Produto.all.paginate(:page => params[:page], :per_page => 5)
+    @produtoss = Produto.all
+    
+    respond_to do |format|      
+      format.json { render json: {produtos: @produtoss} }
       format.html      
     end
   end
