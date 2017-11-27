cf13b428f9f5c44a9f0efd3d3dde1927e1ccb018
jmdinfo123 <jmdinfo123@gmail.com>
2017-11-27 11:10:15 -0500

    algumas alteracoes

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jwt'
 gem 'paperclip'
 
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
 # Use sqlite3 as the database for Active Record
--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -9,8 +9,20 @@ class RelatoriosController < BackofficeController
     @data_fin = DateTime.parse(params[:data_fin]).strftime("%Y-%m-%dT%l:%M:%S%z")
     
     
-    @pedidos = Pedido.where(created_at: @data_ini..@data_fin)
+    @pedidos = Pedido.where(created_at: @data_ini..@data_fin, status: 3)
+    
+    respond_to do |format|
+      format.html
+      format.pdf do
+       render pdf: "file_name_of_your_choice",
+               template: "relatorios/gerar_por_periodo.pdf.erb",
+               orientation:                    'Portrait', #Landscape
+               locals: {:invoice => @invoice}
+      end
+    end
     
   end
+    
+
   
 end
